2ba94e7f333e99b86c667b00fd66582b0fc22c16
kleix5 <pablo.kleix@gmail.com>
2026-04-24 23:27:43 +0300

    Structure Planning

--- cmd/mysite/main.go
+++ cmd/mysite/main.go
@@ -3,74 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	"webstory/internal/app"
 )
 
 const Host = "http://localhost:5000"
 
 func main() {
-	// 1. Указываем, что отдавать файлы из текущей директории
-	fs := http.FileServer(http.Dir("."))
-	http.Handle("./", fs)
+	server := app.NewServer()
 
-	port := "5000"
-	url := Host
+	fmt.Println("Server running on HOST: ", Host)
+	log.Fatal(server.Start(":5000"))
 
-	fmt.Println("Сервер запущен:", url)
-
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	// http.ListenAndServe(":8080", nil)
-
-	// 3. Запуск сервера
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-
-	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		http.Redirect(w, r, "/layout.html", http.StatusSeeOther) // 303
-	})
-	// http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
-	// 	if r.Method != http.MethodPost {
-	// 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-	// 		return
-	// 	}
-	// 	tmpl, err := template.ParseFiles("layout.html")
-	// 	if err != nil {
-	// 		http.Error(w, err.Error(), 400)
-	// 		return
-	// 	}
-	// 	tmpl.Execute(w, nil)
-	// 	if err := tmpl.Execute(w, nil); err != nil {
-	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 		return
-	// 	}
-	// 	// fmt.Fprintln(w, "POST received")
-	// })
-}
-
-// func handler(w http.ResponseWriter, r *http.Request) {
-//     // Парсим и выводим HTML-файл
-//     tmpl, err := template.ParseFiles("index.html")
-//     if err != nil {
-//         http.Error(w, err.Error(), 400)
-//         return
-//     }
-//     tmpl.Execute(w, nil)
-// }
-
-// Обработчик, который принимает POST-запрос от кнопки
-func buttonHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		// Обработка нажатия
-		fmt.Println("Кнопка нажата пользователем!")
-
-		// Отправляем ответ обратно клиенту
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Нажатие обработано"))
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	}
 }
--- internal/app/server.go
+++ internal/app/server.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -8,6 +9,26 @@ type Server struct {
 	mux *http.ServeMux
 }
 
+func NewServer() *Server {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	// Handle the Get Started form submit
+	mux.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		http.Redirect(w, r, "/layout.html", http.StatusSeeOther) // 303
+		fmt.Println("The BUTTON was Clicked!!!")
+	})
+
+	return &Server{mux: mux}
+}
+
 func (s *Server) Start(addr string) error {
 	return http.ListenAndServe(addr, s.mux)
+}
+
+func (s *Server) routes() {
+	s.mux.Handle("/", http.FileServer(http.Dir(".")))
 }
