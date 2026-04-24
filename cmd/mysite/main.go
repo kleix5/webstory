d63c49cc31becb113ce10c3dcf1ba3c1e32f9f57
kleix5 <pablo.kleix@gmail.com>
2026-04-24 16:02:27 +0300

    Last commit

--- cmd/mysite/main.go
+++ cmd/mysite/main.go
@@ -6,23 +6,49 @@ import (
 	"net/http"
 )
 
-const Host = "http://localhost:8080"
+const Host = "http://localhost:5000"
 
 func main() {
 	// 1. Указываем, что отдавать файлы из текущей директории
-	fs := http.FileServer(http.Dir("../../."))
-	http.Handle("../../", fs)
+	fs := http.FileServer(http.Dir("."))
+	http.Handle("./", fs)
 
-	port := "8080"
+	port := "5000"
 	url := Host
 
 	fmt.Println("Сервер запущен:", url)
 
-	http.Handle("/", http.FileServer(http.Dir("../../.")))
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	// http.ListenAndServe(":8080", nil)
 
 	// 3. Запуск сервера
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		http.Redirect(w, r, "/layout.html", http.StatusSeeOther) // 303
+	})
+	// http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.Method != http.MethodPost {
+	// 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	// 		return
+	// 	}
+	// 	tmpl, err := template.ParseFiles("layout.html")
+	// 	if err != nil {
+	// 		http.Error(w, err.Error(), 400)
+	// 		return
+	// 	}
+	// 	tmpl.Execute(w, nil)
+	// 	if err := tmpl.Execute(w, nil); err != nil {
+	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
+	// 	// fmt.Fprintln(w, "POST received")
+	// })
 }
 
 // func handler(w http.ResponseWriter, r *http.Request) {
